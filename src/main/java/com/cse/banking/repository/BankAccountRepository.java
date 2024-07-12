5b57405a043b81a427625dd7ef6e565743d49bd1
Gowri Seelam <165471777+gowriseelamsu@users.noreply.github.com>
2024-07-12 15:43:37 +0530

    Delete src directory

--- src/main/java/com/cse/banking/repository/BankAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cse.banking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cse.banking.entity.BankAccount;
-
-public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
-    
-}
